975a9af6c05326c6d82fdae86ccfab68ea495fbf
newtondotcom <asphalt9fr@gmail.com>
2024-03-24 20:14:11 +0100

    list repos with breadcumb

--- pages/repos.vue
+++ pages/repos.vue
@@ -1,10 +1,29 @@
+<script setup lang="ts">
+      const toast = useToast()
+      const links = [{
+        label: 'Home',
+        icon: 'i-heroicons-home',
+        to: '/'
+        }, {
+        label: 'Repositories',
+        icon: 'i-heroicons-square-3-stack-3d'
+        }]
+
+      onMounted(() => {
+        toast.add({ title: 'Welcome to mgrs!' })
+      })
+</script>
+
+
+
 <template>
 <div class="text-center">
   <h1 class="text-4xl font-bold">Repos</h1>
+  <UBreadcrumb :links="links" />
   <div class="mt-4">
-    <button @click="signOut" class="bg-red-500 text-white px-4 py-2 rounded">
+    <UButton @click="signOut" class="bg-red-500">
       Sign Out
-    </button>
+    </UButton>
   </div>
 </div>
 </template>
