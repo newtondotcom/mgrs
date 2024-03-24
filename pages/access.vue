7d531430999fd8ab2112a5b0850ef0fe82c51e56
newtondotcom <asphalt9fr@gmail.com>
2024-03-24 20:14:31 +0100

    page to ask for access token

---
+++ pages/access.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+const useGithubApp = async () => {
+    let link = "https://github.com/login/oauth/authorize"
+    window.location.href = `${link}?client_id=${config.public.GITHUB_CLIENT_ID}`
+}
+</script>
+
+<template>
+<UButton @click="useGithubApp">Allow us to manage your Github secrets</UButton>
+</template>
