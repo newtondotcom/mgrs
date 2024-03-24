7f47042474c1a4f67c59352f164a223e080d25fd
newtondotcom <asphalt9fr@gmail.com>
2024-03-24 20:19:53 +0100

    individual repo page

---
+++ pages/repo/[id].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const route = useRoute()
+const { id } = route.params.id
+const links = [{
+        label: 'Home',
+        icon: 'i-heroicons-home',
+        to: '/'
+        }, {
+        label: 'Repositories',
+        icon: 'i-heroicons-square-3-stack-3d',
+        to: '/repos'
+        }, {
+  label: 'Repository',
+  icon: 'i-heroicons-link'
+}]
+</script>
+
+<template>
+  <h1 class="text-4xl font-bold">Repo</h1>
+  <UBreadcrumb :links="links" />
+</template>
