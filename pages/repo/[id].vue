c68c5c537562c3b8c72fb8169b9432632acf32c9
newtondotcom <asphalt9fr@gmail.com>
2024-03-25 19:31:38 +0100

    try to list user repo secrets

--- pages/repo/[id].vue
+++ pages/repo/[id].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
+import { Octokit } from "@octokit/core";
 const route = useRoute()
-const { id } = route.params.id
+const name = route.params.id
 const links = [{
         label: 'Home',
         icon: 'i-heroicons-home',
@@ -13,9 +14,26 @@ const links = [{
   label: 'Repository',
   icon: 'i-heroicons-link'
 }]
+
+const access_token_cookie = useCookie('access_token')
+
+const octokit = new Octokit({
+  auth: access_token_cookie.value
+})
+
+await octokit.request('GET /repos/{owner}/{repo}/environments/{environment_name}/secrets', {
+  owner: "newtondotcom",
+  repo: name,
+  environment_name: "",
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
 </script>
 
 <template>
-  <h1 class="text-4xl font-bold">Repo</h1>
+  <div class="text-center">
+    <h1 class="text-4xl font-bold">Repositories</h1>
   <UBreadcrumb :links="links" />
+  </div>
 </template>
