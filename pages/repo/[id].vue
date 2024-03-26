8f9643750a84d1a78f9d1e6f99897d011b322896
newtondotcom <asphalt9fr@gmail.com>
2024-03-26 14:28:42 +0100

    case when no secrets + add secret

--- pages/repo/[id].vue
+++ pages/repo/[id].vue
@@ -11,9 +11,11 @@ const links = [{
         icon: 'i-heroicons-square-3-stack-3d',
         to: '/repos'
         }, {
-  label: 'Repository',
+  label: name,
   icon: 'i-heroicons-link'
 }]
+
+const noSecret = ref(false)
 
 const access_token_cookie = useCookie('access_token')
 
@@ -34,9 +36,11 @@ async function getSecretsList() {
       }
     })
     datas.value = response.data.secrets
+    if (datas.value.length === 0) {
+      noSecret.value = true
+    }
   } catch (error) {
     console.error('Error fetching secrets:', error)
-    toast.error('Error fetching secrets')
   }
 }
 
@@ -64,8 +68,11 @@ await octokit.request('PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}',
   <div class="text-center">
     <h1 class="text-4xl font-bold">Repositories</h1>
     <UBreadcrumb :links="links" class="mb-6 ml-6" />
-    <div class="px-6">
-      <template v-if="datas.length === 0">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-6 lg:py-[60px] lg:px-[100px]">
+      <div v-if="noSecret" class="border border-gray-300 bg-gray-100 p-4 rounded-md mb-4">
+        This repo has no secrets
+      </div>
+      <template v-if="datas.length === 0 && !noSecret">
         <div v-for="n in 6" :key="n" class="border border-gray-300 bg-gray-100 p-4 rounded-md mb-4">
           <div class="flex items-center space-x-4">
             <USkeleton class="h-12 w-12" :ui="{ rounded: 'rounded-full' }" />
@@ -77,31 +84,19 @@ await octokit.request('PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}',
         </div>
       </template>
       <template v-else>
-        <div v-for="data in datas" 
-          :key="data.id" 
-          @click="navigateTo('/repo/' + data.name)"
-          class="border border-gray-300 bg-gray-100 hover:bg-gray-200 hover:border-gray-400 p-4 rounded-md cursor-pointer mb-4">
+        <div v-for="data in datas" :key="data.id" class="border border-gray-300 bg-gray-100 hover:bg-gray-200 hover:border-gray-400 p-4 rounded-md cursor-pointer mb-4">
           {{ data.name }}
-          <div class="flex flex-row justify-center">
+          <div class="flex flex-row justify-center mt-2">
             <UInput color="primary" variant="outline" placeholder="Search..." />
-            <UButton
-              icon="i-heroicons-pencil-square"
-              size="sm"
-              color="primary"
-              square
-              variant="solid"
-            />
-
-            <UButton
-              icon="i-heroicons-check-16-solid"
-              size="sm"
-              color="primary"
-              square
-              variant="solid"
-            />
+            <UButton icon="i-heroicons-eye-16-solid" size="sm" color="primary" square variant="solid" />
+            <UButton icon="i-heroicons-check-16-solid" size="sm" color="primary" square variant="solid" />
           </div>
         </div>
       </template>
+        <div class="border border-gray-300 bg-gray-100 hover:bg-gray-200 hover:border-gray-400 p-4 rounded-md cursor-pointer mb-4">
+            +
+        </div>
     </div>
   </div>
 </template>
+
