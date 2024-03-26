0f22bf4f5d4d0d277347c708d3c28d712de377bd
newtondotcom <asphalt9fr@gmail.com>
2024-03-26 14:48:42 +0100

    avatar component

---
+++ components/Avatar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UPopover>
+  <UButton label="Button" color="gray">
+    <template #leading>
+  <UAvatar
+    chip-color="primary"
+    chip-text=""
+    chip-position="top-right"
+    size="sm"
+    src="https://avatars.githubusercontent.com/u/739984?v=4"
+    alt="Avatar"
+  />
+    </template>
+  </UButton>
+    <template #panel>
+      <div class="p-4">
+        <Placeholder class="h-20 w-48" />
+      </div>
+    </template>
+  </UPopover>
+</template>
