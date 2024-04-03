c558f409c54539ea59d8f2bed0c0ef6a2df0efdf
newtondotcom <asphalt9fr@gmail.com>
2024-04-03 15:46:04 +0200

    page to ask for gh app permissions

--- pages/github.vue
+++ pages/github.vue
@@ -1,23 +1,13 @@
 <script setup lang="ts">
-const config = useRuntimeConfig();
-const route = useRoute().query;
-const code = route.code;
-
-const access_token_cookie = useCookie('access_token');
-const username_cookie = useCookie('username');
-
-const {data} = await useFetch('/api/github',{
-    params: { code : code },
-});
-access_token_cookie.value = data.value.access_token;
-username_cookie.value = data.value.username;
-navigateTo('/repos');
+const config = useRuntimeConfig()
+const useGithubApp = async () => {
+    let link = "https://github.com/login/oauth/authorize"
+    window.location.href = `${link}?client_id=${config.public.GH_CLIENT_ID}&response_type=code&scope=repo,admin:org`
+}
 </script>
 
 <template>
-<div id="loading-screen" class="w-full h-full fixed block top-0 left-0 bg-white opacity-75 z-50">
-  <span class="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0">
-    <i class="fas fa-circle-notch fa-spin fa-5x"></i>
-  </span>
-</div>
+  <div class="w-full h-full flex justify-center items-center">
+    <UButton @click="useGithubApp">Allow us to manage your Github secrets</UButton>
+  </div>
 </template>
