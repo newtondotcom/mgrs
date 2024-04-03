d03a8d2b4c1d56962eff6e81b107359d90d47e65
newtondotcom <asphalt9fr@gmail.com>
2024-04-03 16:01:07 +0200

    theming colors

--- app.vue
+++ app.vue
@@ -9,7 +9,7 @@ if (user.value) {
 <template>
 <header class="bg-white mb-[20px]">
   <div class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-    <a class="block text-teal-600" href="#">
+    <a class="block text-primary-500" href="#">
       <span class="sr-only">Home</span>
       <svg class="h-8" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
@@ -69,7 +69,7 @@ if (user.value) {
 
 <footer class="bg-white">
   <div class="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
-    <div class="flex justify-center text-teal-600">
+    <div class="flex justify-center text-primary-500">
       <svg class="h-8" viewBox="0 0 118 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M37.83 19.2047C37.2352 19.237 36.6469 19.0679 36.16 18.7247C35.9566 18.5739 35.7929 18.3758 35.6831 18.1476C35.5733 17.9193 35.5208 17.6678 35.53 17.4147V8.1447C35.5252 8.1055 35.5293 8.0656 35.5422 8.0282C35.555 7.9908 35.5762 7.9569 35.6042 7.9289C35.6322 7.9009 35.6661 7.8797 35.7035 7.8669C35.7409 7.854 35.7808 7.8499 35.82 7.8547H37.5C37.69 7.8547 37.78 7.9547 37.78 8.1447V16.6947C37.78 17.0747 37.95 17.2647 38.3 17.2647C38.4484 17.2708 38.5968 17.254 38.74 17.2147C38.94 17.2147 39.05 17.2747 39.06 17.4547L39.21 18.7047C39.2172 18.7412 39.2165 18.7787 39.208 18.8149C39.1995 18.851 39.1833 18.885 39.1605 18.9143C39.1378 18.9437 39.109 18.9679 39.0762 18.9852C39.0433 19.0025 39.0071 19.0126 38.97 19.0147C38.602 19.1363 38.2175 19.2004 37.83 19.2047Z"
@@ -115,35 +115,8 @@ if (user.value) {
     </div>
 
     <p class="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt consequuntur amet culpa cum
-      itaque neque.
+      Made with ❤️ in Paris 🥐
     </p>
-
-    <ul class="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
-      <li>
-        <a class="text-gray-700 transition hover:text-gray-700/75" href="#"> About </a>
-      </li>
-
-      <li>
-        <a class="text-gray-700 transition hover:text-gray-700/75" href="#"> Careers </a>
-      </li>
-
-      <li>
-        <a class="text-gray-700 transition hover:text-gray-700/75" href="#"> History </a>
-      </li>
-
-      <li>
-        <a class="text-gray-700 transition hover:text-gray-700/75" href="#"> Services </a>
-      </li>
-
-      <li>
-        <a class="text-gray-700 transition hover:text-gray-700/75" href="#"> Projects </a>
-      </li>
-
-      <li>
-        <a class="text-gray-700 transition hover:text-gray-700/75" href="#"> Blog </a>
-      </li>
-    </ul>
 
     <ul class="mt-12 flex justify-center gap-6 md:gap-8">
       <li>
@@ -151,7 +124,7 @@ if (user.value) {
           href="#"
           rel="noreferrer"
           target="_blank"
-          class="text-gray-700 transition hover:text-gray-700/75"
+          class="text-primary-400 transition hover:text-primary-400/75"
         >
           <span class="sr-only">Facebook</span>
           <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
@@ -169,7 +142,7 @@ if (user.value) {
           href="#"
           rel="noreferrer"
           target="_blank"
-          class="text-gray-700 transition hover:text-gray-700/75"
+          class="text-primary-400 transition hover:text-primary-400/75"
         >
           <span class="sr-only">Instagram</span>
           <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
@@ -187,7 +160,7 @@ if (user.value) {
           href="#"
           rel="noreferrer"
           target="_blank"
-          class="text-gray-700 transition hover:text-gray-700/75"
+          class="text-primary-400 transition hover:text-primary-400/75"
         >
           <span class="sr-only">Twitter</span>
           <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
@@ -203,7 +176,7 @@ if (user.value) {
           href="#"
           rel="noreferrer"
           target="_blank"
-          class="text-gray-700 transition hover:text-gray-700/75"
+          class="text-primary-400 transition hover:text-primary-400/75"
         >
           <span class="sr-only">GitHub</span>
           <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
@@ -221,7 +194,7 @@ if (user.value) {
           href="#"
           rel="noreferrer"
           target="_blank"
-          class="text-gray-700 transition hover:text-gray-700/75"
+          class="text-primary-400 transition hover:text-primary-400/75"
         >
           <span class="sr-only">Dribbble</span>
           <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
