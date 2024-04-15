679d4105bffc6af537a1b1af8df33aa18ebd30ca
newtondotcom <asphalt9fr@gmail.com>
2024-04-15 22:42:27 +0200

    cleaning

--- server/data/repos.ts
+++ server/data/repos.ts
@@ -4,16 +4,15 @@ export async function getRepos(user_id : string) {
     return await prisma.repository.findMany({
         where: { user_id },
     })
-    
 }
 
-export async function mergeRepos(ghRepos, savedRepos, user_id : string){
+export async function mergeRepos(ghRepos :any[], user_id : string){
     const savedRepos = await getRepos(user_id);
     for (let i = 0; i < ghRepos.length; i++) {
         const ghRepo = ghRepos[i];
         const savedRepo = savedRepos.find((repo) => repo.repository_id === ghRepo.id);
         if (savedRepo) {
-           if (savedRepo.repo_name !== ghRepo.name) {
+           if (savedRepo.repository_name !== ghRepo.name) {
                await prisma.repository.update({
                    where: { repository_id: savedRepo.id },
                    data: { repository_name: ghRepo.name },
