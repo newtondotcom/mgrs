b2139ce9b825b39b21fd077b4cd060adfb4d245f
newtondotcom <asphalt9fr@gmail.com>
2024-04-15 19:26:51 +0200

    format

--- server/api/repos.ts
+++ server/api/repos.ts
@@ -4,21 +4,21 @@ import { getToken } from '../data/user';
 import { Octokit } from "@octokit/core";
 
 export default defineEventHandler(async (event) => {
-const user = await serverSupabaseUser(event)
-const token = await getToken(user.id)
-const octokit = new Octokit({
-    auth: token
-});
+    const user = await serverSupabaseUser(event)
+    const token = await getToken(user.id)
+    const octokit = new Octokit({
+        auth: token
+    });
 
-const response = await octokit.request('GET /user/repos', {
-    type: "owner",
-    headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-    },
-    per_page: 1000
-});
-const ghRepos = response.data;
-mergeRepos(ghRepos, user.id);
-return ghRepos;
+    const response = await octokit.request('GET /user/repos', {
+        type: "owner",
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        },
+        per_page: 1000
+    });
+    const ghRepos = response.data;
+    mergeRepos(ghRepos, user.id);
+    return ghRepos;
 });
 
