b05554d85de1a8a7e9b92494bd4b2afdf404fe7d
newtondotcom <asphalt9fr@gmail.com>
2024-04-15 17:55:01 +0200

    api endpoint to get all repo and launch asynchronous trigger of fct to merge repos

---
+++ server/api/repos.ts
@@ -0,0 +1,26 @@
+import { serverSupabaseUser } from '#supabase/server'
+import { getRepos,mergeRepos } from '../data/repos';
+import { getToken } from '../data/user';
+import { Octokit } from "@octokit/core";
+
+export default defineEventHandler(async (event) => {
+
+const user = await serverSupabaseUser(event)
+const token = await getToken(user.id)
+const octokit = new Octokit({
+    auth: token
+});
+
+const response = await octokit.request('GET /user/repos', {
+    type: "owner",
+    headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+    },
+    per_page: 1000
+});
+const ghRepos = response.data;
+const savedRepos = await getRepos(user.id);
+mergeRepos(ghRepos, savedRepos,user.id);
+return ghRepos;
+});
+
