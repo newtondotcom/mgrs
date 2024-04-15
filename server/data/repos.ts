c04f64cb9a6388bfbfcb06edf4052d2202791eb5
newtondotcom <asphalt9fr@gmail.com>
2024-04-15 17:56:00 +0200

    prima functions about repos

---
+++ server/data/repos.ts
@@ -0,0 +1,31 @@
+import prisma from "./prisma";
+
+export async function getRepos(user_id : string) {
+    return await prisma.repository.findMany({
+        where: { user_id },
+    })
+    
+}
+
+export async function mergeRepos(ghRepos, savedRepos, user_id : string){
+    for (let i = 0; i < ghRepos.length; i++) {
+        const ghRepo = ghRepos[i];
+        const savedRepo = savedRepos.find((repo) => repo.repository_id === ghRepo.id);
+        if (savedRepo) {
+           if (savedRepo.repo_name !== ghRepo.name) {
+               await prisma.repository.update({
+                   where: { repository_id: savedRepo.id },
+                   data: { repository_name: ghRepo.name },
+               })
+           }
+        } else {
+            await prisma.repository.create({
+                data: {
+                    repository_id: ghRepo.id,
+                    repository_name: ghRepo.name,
+                    user_id,
+                },
+            })
+        }
+    }
+   }
