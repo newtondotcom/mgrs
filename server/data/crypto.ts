376989672a29e02c7131ee7bb778ae1dc075d301
newtondotcom <asphalt9fr@gmail.com>
2024-04-15 23:31:03 +0200

    transform to  buffer

--- server/data/crypto.ts
+++ server/data/crypto.ts
@@ -2,18 +2,20 @@ import crypto from 'crypto';
 import sodium from "libsodium-wrappers";
 
 export function encrypt(plaintext: string, key: string): string {
+    const keyBuffer = Buffer.from(key, 'hex');
     const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    const cipher = crypto.createCipheriv('aes-256-cbc', keyBuffer, iv);
     let encrypted = cipher.update(plaintext, 'utf8', 'base64');
     encrypted += cipher.final('base64');
     return iv.toString('hex') + ':' + encrypted;
 }
 
 export function decrypt(ciphertext: string, key: string): string {
+    const keyBuffer = Buffer.from(key, 'hex');
     const parts = ciphertext.split(':');
     const iv = Buffer.from(parts[0], 'hex');
     const encryptedText = parts[1];
-    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    const decipher = crypto.createDecipheriv('aes-256-cbc', keyBuffer, iv);
     let decrypted = decipher.update(encryptedText, 'base64', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
