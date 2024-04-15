632112d9e84a535a470bb0e322054af750f474fa
newtondotcom <asphalt9fr@gmail.com>
2024-04-15 17:55:46 +0200

    prisma functions about user

--- server/data/user.ts
+++ server/data/user.ts
@@ -1,16 +1,29 @@
 import prisma from "~/server/data/prisma"
 
-export async function registerUser(user_id : string, access_token :string) {
+export async function registerUser(user_id : string, access_token :string, username : string, avatar_url : string) {
+    console.log(username)
     const userPrisma = await prisma.user.upsert({
         where: { user_id },
-        update: { access_token },
-        create: { user_id, access_token }
+        update: { access_token , username, avatar_url},
+        create: { user_id, access_token , username, avatar_url},
     })
 }
 
-export async function getTokens(user_id : string) {
+export async function getToken(user_id : string) {
     return await prisma.user.findUnique({
         where: { user_id },
-        select: { access_token: true }
+        select : {
+            access_token : true
+        }
+    })
+}
+
+export async function getInfos(user_id : string) {
+    return await prisma.user.findUnique({
+        where: { user_id },
+        select : {
+            username : true,
+            avatar_url : true
+        }
     })
 }
