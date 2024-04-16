5bd4b413ebb447d1208eef849fa08a081d0353b4
newtondotcom <asphalt9fr@gmail.com>
2024-04-17 00:46:15 +0200

    page to get logo

---
+++ pages/logo.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="flex justify-center text-primary-500">
+    <a class="block text-primary-500" href="/">
+      <span class="sr-only">Home</span>
+      <UIcon name="i-heroicons-lock-closed" class='w-10 h-10' />
+      <span class="text-2xl font-bold">MGRS</span>
+    </a>
+<div class="mt-[150px] mb-[150px]">
+    <UIcon name="i-heroicons-lock-closed" class='text-primary-500 w-[100px] h-[100px]' />
+</div>
+  </div>
+</template>
