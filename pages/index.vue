275d0f0e543e382c3c0b80ff14f21bbca5e4e4fa
newtondotcom <asphalt9fr@gmail.com>
2024-04-16 17:23:01 +0200

    added landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,30 @@
-<template>
+<script setup lang="ts">
+function goDashoard() {
+    navigateTo('/dashboard')
+}
+</script>
 
+<template>
+    <section class="overflow-hidden sm:grid sm:grid-cols-2 sm:items-center mb-[70px]">
+        <div class="p-8 md:p-12 lg:px-16 lg:py-24">
+            <div class="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+                <h2 class="text-2xl font-bold text-gray-900 md:text-3xl dark:text-white">
+                    A tool to manage your Github personal Repositories secrets
+                </h2>
+
+                <p class="hidden text-gray-500 md:mt-4 md:block dark:text-gray-400">
+                    An open-source web app simply made to manage your Github personal Repositories secrets with no <span
+                        class="italic font-semibold">fioritures</span> (from French, meaning <span
+                        class="italic">frills</span>).
+                </p>
+
+                <UButton class="mt-4" @click="goDashoard">
+                    Get Started
+                </UButton>
+            </div>
+        </div>
+
+        <img alt="" src="~/public/secrets.png"
+            class="h-full w-full sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[20px]" />
+    </section>
 </template>
