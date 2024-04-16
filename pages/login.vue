d6308be3d38c12bc0c1924471b05b0adc24b0904
newtondotcom <asphalt9fr@gmail.com>
2024-04-16 22:47:48 +0200

    dont provide redirect url

--- pages/login.vue
+++ pages/login.vue
@@ -4,10 +4,7 @@ const email = ref('')
 
 const signInWithGithub = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: 'github',
-    options: {
-        redirectTo: 'http://localhost:3000/auth'
-    }
+    provider: 'github'
     })
     if (error) console.log(error)
 }
